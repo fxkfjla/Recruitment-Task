a2d793031b7aa05a506aade94226aa1fc44bd5b1
fxkfjla <kacper.skr11@gmail.com>
2023-07-18 18:46:13 +0200

    initial commit

---
+++ src/test/java/com/example/recruitmenttask/RecruitmenttaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recruitmenttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmenttaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
