bf510f70222dd4e5d11f45afb4e775588f70cb16
fxkfjla <kacper.skr11@gmail.com>
2023-07-23 19:58:34 +0200

    add getter and setter for id

--- src/main/java/com/example/recruitmenttask/Models/User.java
+++ src/main/java/com/example/recruitmenttask/Models/User.java
@@ -20,9 +20,11 @@ public class User
 	}
 	
 	// Getters, setters
+	public Long getId() { return id; }
 	public String getName() { return name; }
 	public String getSurname() { return surname; }
 	public String getLogin() { return login; }
+	public void setId(Long id) { this.id = id; }
 	public void setName(String name) { this.name = name; }
 	public void setSurname(String surname) { this.surname = surname; }
 	public void setLogin(String login) { this.login = login; }
