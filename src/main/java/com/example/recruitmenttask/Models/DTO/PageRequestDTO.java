82a838ae6a96ac30d602ef2579b510e555de9c2d
fxkfjla <kacper.skr11@gmail.com>
2023-07-28 17:27:15 +0200

    override hashCode and equals methods in PageRequestDTO
    in order to allow proper usage of PageRequestDTO objects in hash maps
    and etc. it needs to have hashCode and equals methods properly overridden

--- src/main/java/com/example/recruitmenttask/Models/DTO/PageRequestDTO.java
+++ src/main/java/com/example/recruitmenttask/Models/DTO/PageRequestDTO.java
@@ -1,5 +1,7 @@
 package com.example.recruitmenttask.Models.DTO;
 
+import java.util.Objects;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -8,6 +10,14 @@ import javax.validation.constraints.Pattern.Flag;
 
 public class PageRequestDTO
 {
+	public PageRequestDTO(int page, int size, String direction, String field)
+	{
+		this.page = page;
+		this.size = size;
+		this.direction = direction;
+		this.field = field;
+	}
+	
 	// Getters, setters
 	public int getPage() { return page; }
 	public int getSize() { return size; }
@@ -17,6 +27,27 @@ public class PageRequestDTO
 	public void setSize(int size) { this.size = size; }
 	public void setDirection(String direction) { this.direction = direction; }
 	public void setField(String field) { this.field = field; }
+	
+	
+	@Override
+    public boolean equals(Object o)
+	{
+        if (this == o) return true;
+        if (!(o instanceof PageRequestDTO)) return false;
+        
+        PageRequestDTO rhs = (PageRequestDTO)o;
+        
+        return getPage() == rhs.getPage() &&
+        		getSize() == rhs.getSize() &&
+        		getDirection().equals(rhs.getDirection()) &&
+        		getField().equals(rhs.getField());
+    }
+		
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(getPage(), getSize(), getDirection(), getField());
+    }
 	
 	@NotNull
 	@Min(0)
